9d05325b040e17c5a7e8da6a2778bff073a49b32
solbarda <sorubaruda@gmail.com>
2020-05-12 10:32:28 +0200

    Added imgui menubar

--- DraconicGB/Source/DraconicEmulator.cpp
+++ DraconicGB/Source/DraconicEmulator.cpp
@@ -57,7 +57,7 @@ int DraconicEmulator::Init()
   SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
   SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
   SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
-  window = SDL_CreateWindow("Dear ImGui SDL2+OpenGL3 example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
+  window = SDL_CreateWindow("Draconic GB Emulator", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
   gl_context = SDL_GL_CreateContext(window);
   SDL_GL_MakeCurrent(window, gl_context);
   SDL_GL_SetSwapInterval(1); // Enable vsync
@@ -149,6 +149,99 @@ void DraconicEmulator::DebugRender()
   ImGui_ImplSDL2_NewFrame(window);
   ImGui::NewFrame();
 
+
+  if (ImGui::BeginMainMenuBar())
+  {
+    if (ImGui::BeginMenu("File"))
+    {
+      ImGui::MenuItem("(dummy menu)", NULL, false, false);
+      if (ImGui::MenuItem("New")) {}
+      if (ImGui::MenuItem("Open", "Ctrl+O")) {}
+      if (ImGui::BeginMenu("Open Recent"))
+      {
+        ImGui::MenuItem("fish_hat.c");
+        ImGui::MenuItem("fish_hat.inl");
+        ImGui::MenuItem("fish_hat.h");
+        if (ImGui::BeginMenu("More.."))
+        {
+          ImGui::MenuItem("Hello");
+          ImGui::MenuItem("Sailor");
+          if (ImGui::BeginMenu("Recurse.."))
+          {
+            //ShowExampleMenuFile();
+            ImGui::EndMenu();
+          }
+          ImGui::EndMenu();
+        }
+        ImGui::EndMenu();
+      }
+      if (ImGui::MenuItem("Save", "Ctrl+S")) {}
+      if (ImGui::MenuItem("Save As..")) {}
+
+      ImGui::Separator();
+      if (ImGui::BeginMenu("Options"))
+      {
+        static bool enabled = true;
+        ImGui::MenuItem("Enabled", "", &enabled);
+        ImGui::BeginChild("child", ImVec2(0, 60), true);
+        for (int i = 0; i < 10; i++)
+          ImGui::Text("Scrolling Text %d", i);
+        ImGui::EndChild();
+        static float f = 0.5f;
+        static int n = 0;
+        ImGui::SliderFloat("Value", &f, 0.0f, 1.0f);
+        ImGui::InputFloat("Input", &f, 0.1f);
+        ImGui::Combo("Combo", &n, "Yes\0No\0Maybe\0\0");
+        ImGui::EndMenu();
+      }
+
+      if (ImGui::BeginMenu("Colors"))
+      {
+        float sz = ImGui::GetTextLineHeight();
+        for (int i = 0; i < ImGuiCol_COUNT; i++)
+        {
+          const char* name = ImGui::GetStyleColorName((ImGuiCol)i);
+          ImVec2 p = ImGui::GetCursorScreenPos();
+          ImGui::GetWindowDrawList()->AddRectFilled(p, ImVec2(p.x + sz, p.y + sz), ImGui::GetColorU32((ImGuiCol)i));
+          ImGui::Dummy(ImVec2(sz, sz));
+          ImGui::SameLine();
+          ImGui::MenuItem(name);
+        }
+        ImGui::EndMenu();
+      }
+
+      // Here we demonstrate appending again to the "Options" menu (which we already created above)
+      // Of course in this demo it is a little bit silly that this function calls BeginMenu("Options") twice.
+      // In a real code-base using it would make senses to use this feature from very different code locations.
+      if (ImGui::BeginMenu("Options")) // <-- Append!
+      {
+        static bool b = true;
+        ImGui::Checkbox("SomeOption", &b);
+        ImGui::EndMenu();
+      }
+
+      if (ImGui::BeginMenu("Disabled", false)) // Disabled
+      {
+        IM_ASSERT(0);
+      }
+      if (ImGui::MenuItem("Checked", NULL, true)) {}
+      if (ImGui::MenuItem("Quit", "Alt+F4")) {}
+      ImGui::EndMenu();
+    }
+    if (ImGui::BeginMenu("Edit"))
+    {
+      if (ImGui::MenuItem("Undo", "CTRL+Z")) {}
+      if (ImGui::MenuItem("Redo", "CTRL+Y", false, false)) {}  // Disabled item
+      ImGui::Separator();
+      if (ImGui::MenuItem("Cut", "CTRL+X")) {}
+      if (ImGui::MenuItem("Copy", "CTRL+C")) {}
+      if (ImGui::MenuItem("Paste", "CTRL+V")) {}
+      ImGui::EndMenu();
+    }
+    ImGui::EndMainMenuBar();
+  }
+
+  /*
   // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
   if (show_demo_window)
     ImGui::ShowDemoWindow(&show_demo_window);
@@ -187,6 +280,8 @@ void DraconicEmulator::DebugRender()
       show_another_window = false;
     ImGui::End();
   }
+
+  */
 
   // Rendering
   ImGui::Render();
